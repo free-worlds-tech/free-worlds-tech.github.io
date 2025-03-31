a4bfa77216927b21318a78ccb7ea148484371f32
Scott Boehmer <scottboehmer@hotmail.com>
2025-03-31 17:23:41 -0400

    generate site nav to make it easier to keep consistent across all pages

---
+++ script/fwti.js
@@ -0,0 +1,46 @@
+initializeSiteNav();
+
+function initializeSiteNav() {
+    const siteNav = document.getElementById("site-nav");
+    const currentPage = siteNav.dataset.current;
+
+    const homeNav = document.createElement("a");
+    homeNav.innerText = "Home";
+    if (currentPage == "home") {
+        homeNav.className = "current";
+        homeNav.href = "#";
+    } else {
+        homeNav.href = "index.html";
+    }
+    siteNav.appendChild(homeNav);
+
+    const forceBuilderNav = document.createElement("a");
+    forceBuilderNav.innerText = "Force Builder";
+    if (currentPage == "force-builder") {
+        forceBuilderNav.className = "current";
+        forceBuilderNav.href = "#";
+    } else {
+        forceBuilderNav.href = "force-builder.html";
+    }
+    siteNav.appendChild(forceBuilderNav);
+
+    const firingArcsNav = document.createElement("a");
+    firingArcsNav.innerText = "Firing Arcs";
+    if (currentPage == "firing-arcs") {
+        firingArcsNav.className = "current";
+        firingArcsNav.href = "#";
+    } else {
+        firingArcsNav.href = "firing-arcs-diagram.html";
+    }
+    siteNav.appendChild(firingArcsNav);
+
+    const innerSphereNav = document.createElement("a");
+    innerSphereNav.innerText = "Inner Sphere";
+    if (currentPage == "inner-sphere") {
+        innerSphereNav.className = "current";
+        innerSphereNav.href = "#";
+    } else {
+        innerSphereNav.href = "the-inner-sphere.html";
+    }
+    siteNav.appendChild(innerSphereNav);
+}
